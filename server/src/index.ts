344ea3efaa8dfdfae6fff6d63453e5482fe06b20
bariso0 <53085074+barisbargun@users.noreply.github.com>
2024-04-29 22:40:30 +0300

    refactor: added rate-limit and csrf

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,9 @@ import { SupabaseClient } from "@supabase/supabase-js";
 import dbConnect from '@/config/dbConnect';
 import routes from './routes';
 import { corsOptions } from './config';
+import { rateLimiter } from './config/rateLimit';
+import { csrf } from 'hono/csrf'
+import csrfOptions from './config/csrfOptions';
 
 declare module "hono" {
   interface ContextVariableMap {
@@ -12,9 +15,12 @@ declare module "hono" {
 }
 const app = new Hono<{ Bindings: Bindings }>()
 
-app.use("*",corsOptions());
+app
+  .use("*", corsOptions())
+  .use(csrfOptions)
+  .use("*", rateLimiter)
+  .use(dbConnect)
+  .route("/", routes)
 
-app.use(dbConnect);
-app.route("/", routes)
 
 export default app;
