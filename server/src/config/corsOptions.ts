18fc0090dd30818071e592e0c37681be5524e72e
bariso0 <53085074+barisbargun@users.noreply.github.com>
2024-04-29 11:44:13 +0300

    fix: cors only allows localhost on dev

--- server/src/config/corsOptions.ts
+++ server/src/config/corsOptions.ts
@@ -6,8 +6,9 @@ import { HTTPException } from 'hono/http-exception';
 const corsOptions = () =>
   cors({
     origin: (origin, c: Context<{ Bindings: Bindings }>) => {
-      if(!origin && c.env.NODE_ENV != "dev") throw new HTTPException(502, { message: 'NOT ALLOWED BY CORS', cause: "CORS" })
-      return origin == "http://localhost:5173" ? origin : c.env.CLIENT_URL;
+      if(c.env.NODE_ENV == "dev") return origin;
+      if(!origin) throw new HTTPException(502, { message: 'NOT ALLOWED BY CORS', cause: "CORS" })
+      return c.env.CLIENT_URL;
     },
     allowMethods: ['POST', 'GET', 'OPTIONS', "PATCH", "DELETE"],
     credentials: true,
